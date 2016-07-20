a9d2331f8d25ac8351325898d034032bef71ed9d
Alex Bryant <arbryant@sae.au>
2016-07-20 15:54:31 +1000

    Added a pause menu - Kellem

---
+++ Roller Derby/Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UI_Manager : MonoBehaviour {
+
+	public GameObject pauseMenu;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.P)) 
+		{
+			if (Time.timeScale == 1) {
+				pauseMenu.SetActive (true);
+				Time.timeScale = 0;
+
+			} 
+			else 
+			{
+				Time.timeScale = 1;
+				pauseMenu.SetActive (false);
+			}
+		}
+	}
+
+	public void Quit()
+	{
+		SceneManager.LoadScene ("Main Menu");
+	}
+}
