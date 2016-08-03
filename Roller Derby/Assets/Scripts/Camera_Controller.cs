e7d0f3c4d81545bb09587eac808a01080c0a0f74
Alex Bryant <arbryant@sae.au>
2016-08-03 13:32:12 +1000

    Sprint 1 components merge Untested so far

---
+++ Roller Derby/Assets/Scripts/Camera_Controller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Controller : MonoBehaviour {
+
+    public GameObject player;
+    public float moveSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    Vector3 newPos = new Vector3();
+        //newPos.x = Mathf.Lerp(transform.position.x, player.transform.position.x, Time.deltaTime * moveSpeed);
+        //newPos.y = Mathf.Lerp(transform.position.y, player.transform.position.y + 4.6f, Time.deltaTime * moveSpeed);
+        newPos.x = player.transform.position.x;
+        newPos.y = player.transform.position.y + 4.6f;
+        newPos.z = -10;
+        transform.position = newPos;
+	}
+}
---
+++ Roller Derby/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Player_Controller : MonoBehaviour {
+
+    public float moveSpeed;
+    float maxSpeed = 10000;
+    float minSpeed = -250;
+    public float accSpeed;
+    public Slider Speed;
+    public GameObject chassis;
+    public GameObject[] wheels;
+    public Vector3 chassisPos = new Vector3();
+    public float angle;
+    float moveAmount;
+    float prevPos = 0;
+
+    /*
+    Wheels[0]: Left Back
+    Wheels[1]: Left Front
+    Wheels[2]: Right Back
+    Wheels[3]: Right Front
+    */
+
+    // Use this for initialization
+    void Start() {
+        prevPos = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        moveAmount = Mathf.Round(transform.position.x * 100) - Mathf.Round(prevPos * 100);
+        Debug.Log (moveAmount);
+        moveSpeed += Speed.value * 100;
+        moveSpeed = Mathf.Clamp(moveSpeed, minSpeed, maxSpeed);
+
+        foreach (GameObject wheel in wheels) {
+            if ((moveAmount > 0 && Speed.value < 0) || (moveAmount < 0 && Speed.value > 0)) {
+                wheel.GetComponent<WheelCollider>().brakeTorque = maxSpeed * 10;
+                moveSpeed = 0;
+                wheel.GetComponent<WheelCollider>().motorTorque = moveSpeed;
+            }
+            else {
+                wheel.GetComponent<WheelCollider>().motorTorque = moveSpeed;
+                //Debug.Log("MoveSpeed: " + moveSpeed + " BrakeTorque: " + wheel.GetComponent<WheelCollider>().brakeTorque);
+                if (wheel.GetComponent<WheelCollider>().brakeTorque > 0) {
+                    wheel.GetComponent<WheelCollider>().brakeTorque -= maxSpeed;
+                }
+            }
+        }
+        prevPos = transform.position.x;
+    }
+
+}
