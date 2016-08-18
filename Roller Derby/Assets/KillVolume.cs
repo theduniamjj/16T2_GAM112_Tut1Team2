a9e005c2e931db38128dc27f366754aa4c27df88
Jacob Duniam <djacob@sae.au>
2016-08-18 11:48:06 +1000

    Commit the changes for Jacob

---
+++ Roller Derby/Assets/KillVolume.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class KillVolume : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void onTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("End");
+        }
+    }
+}
