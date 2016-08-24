35d9c99bfc37cac90b08e795dace46476d908603
Jacob Duniam <djacob@sae.au>
2016-08-24 14:45:55 +1000

    Fixed High-Scores
    ~YAY

--- Roller Derby/Assets/Scripts/EndText.cs
+++ Roller Derby/Assets/Scripts/EndText.cs
@@ -6,11 +6,9 @@ public class EndText : MonoBehaviour {
     public Text thisText;
     public int highScoreCandidate;
 
-    public Text highScore1;
-    public Text highScore2;
-    public Text highScore3;
-    public Text highScore4;
-    public Text highScore5;
+    public float[] highScores;
+
+    public Text[] highScoreTexts;
 
     // Use this for initialization
     void Start () {
@@ -29,45 +27,58 @@ public class EndText : MonoBehaviour {
     {
         highScoreCandidate = 0;
 
-        // The shittiest implementation of a score-placing mecanism you've ever seen D:
-        if (PlayerPrefs.HasKey("HighScore1"))
+        float endTime = PlayerPrefs.GetFloat("EndTime", 0);
+
+        float highScore1 = PlayerPrefs.GetFloat("HighScore1", 5.00f);
+        float highScore2 = PlayerPrefs.GetFloat("HighScore2", 4.00f);
+        float highScore3 = PlayerPrefs.GetFloat("HighScore3", 3.00f);
+        float highScore4 = PlayerPrefs.GetFloat("HighScore4", 2.00f);
+        float highScore5 = PlayerPrefs.GetFloat("HighScore5", 1.00f);
+
+        highScores = new float[] { highScore1, highScore2, highScore3, highScore4, highScore5};
+
+        Debug.Log("EndTime is: " + endTime);
+
+        if (endTime > highScores[0])
         {
-            if (PlayerPrefs.GetFloat("EndTime") > PlayerPrefs.GetFloat("HighScore1"))
-                highScoreCandidate = 1;
-            else if (PlayerPrefs.HasKey("HighScore2"))
-            {
-                if (PlayerPrefs.GetFloat("EndTime") > PlayerPrefs.GetFloat("HighScore2"))
-                    highScoreCandidate = 2;
-            }
-            else if (PlayerPrefs.HasKey("HighScore3"))
-            {
-                if (PlayerPrefs.GetFloat("EndTime") > PlayerPrefs.GetFloat("HighScore3"))
-                    highScoreCandidate = 3;
-            }
-            else if (PlayerPrefs.HasKey("HighScore4"))
-            {
-                if (PlayerPrefs.GetFloat("EndTime") > PlayerPrefs.GetFloat("HighScore4"))
-                    highScoreCandidate = 4;
-            }
-            else if (PlayerPrefs.HasKey("HighScore5"))
-            {
-                if (PlayerPrefs.GetFloat("EndTime") > PlayerPrefs.GetFloat("HighScore5"))
-                    highScoreCandidate = 5;
-            }
-            }
-        Debug.Log("EndTime is: " + PlayerPrefs.GetFloat("EndTime"));
+            highScores[4] = highScores[3];
+            highScores[3] = highScores[2];
+            highScores[2] = highScores[1];
+            highScores[1] = highScores[0];
+            highScores[0] = endTime;
+        }
+        else if (endTime > highScores[1])
+        {
+                highScores[4] = highScores[3];
+                highScores[3] = highScores[2];
+                highScores[2] = highScores[1];
+                highScores[1] = endTime;
+        }
+        else if (endTime > highScores[2])
+        {
+            highScores[4] = highScores[3];
+            highScores[3] = highScores[2];
+            highScores[2] = endTime;
+        }
+        else if (endTime > highScores[3])
+        {
+            highScores[4] = highScores[3];
+            highScores[3] = endTime;
+        }
+        else if (endTime > highScores[3])
+        {
+            highScores[4] = endTime;
+        }
 
-        // Trust me, it gets worse :(
-        if (highScoreCandidate == 1)
-            highScore1.text = PlayerPrefs.GetFloat("EndTime").ToString();
-        else if (highScoreCandidate == 2)
-            highScore2.text = PlayerPrefs.GetFloat("EndTime").ToString();
-        else if (highScoreCandidate == 3)
-            highScore3.text = PlayerPrefs.GetFloat("EndTime").ToString();
-        else if (highScoreCandidate == 4)
-            highScore4.text = PlayerPrefs.GetFloat("EndTime").ToString();
-        else if (highScoreCandidate == 5)
-            highScore5.text = PlayerPrefs.GetFloat("EndTime").ToString();
+        for (int i = 0; i < highScores.Length; i++)
+        {
+            highScoreTexts[i].text = highScores[i] + "s";
+        }
 
+        PlayerPrefs.SetFloat("HighScore1", highScores[0]);
+        PlayerPrefs.SetFloat("HighScore2", highScores[1]);
+        PlayerPrefs.SetFloat("HighScore3", highScores[2]);
+        PlayerPrefs.SetFloat("HighScore4", highScores[3]);
+        PlayerPrefs.SetFloat("HighScore5", highScores[4]);
     }
 }
